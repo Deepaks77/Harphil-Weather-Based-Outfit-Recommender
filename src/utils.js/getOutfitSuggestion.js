4cda8088b1569014219b0d6fde0918293598cf49
Deepak Sindhwani <dsindhwani55@gmail.com>
2025-07-05 22:03:10 +0530

    condition fixed

--- src/utils.js/getOutfitSuggestion.js
+++ src/utils.js/getOutfitSuggestion.js
@@ -1,7 +1,8 @@
 export const getOutfitSuggestion = (condition, temp) => {
-  if (condition.includes("rain")) return "Take an umbrella";
-  if (condition.includes("sun")) return "Wear sunglasses";
-  if (condition.includes("cloud")) return "wear lightweight jacket";
+  if (condition?.toLowerCase().includes("rain")) return "Take an umbrella";
+  if (condition.toLowerCase().includes("sun")) return "Wear sunglasses";
+  if (condition.toLowerCase().includes("cloud"))
+    return "wear lightweight jacket";
   if (temp < 10) return "Wear a jacket";
   if (temp > 30) return "Light clothes recommended";
   return "Dress comfortably";
